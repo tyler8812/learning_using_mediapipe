913a800d88eb13f3b022a9ac47f5a66f1e465197
tyler <tyler88128949@gmail.com>
2021-06-28 15:38:49 +0800

    finish_hand_tracking

--- handTracking/HandTrackingMin.py
+++ handTracking/HandTrackingMin.py
@@ -8,6 +8,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
@@ -20,7 +22,23 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id, lm)
+                height, width, channel = img.shape
+                cx, cy = int(lm.x * width), int(lm.y * height)
+
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(
+        img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 2
+    )
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
